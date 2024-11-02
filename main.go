93bb25adc1ea6c1ce76c88fc014c93dec5af8e13
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-11-03 00:43:20 +0100

    init Gin project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, Gin!")
+	})
+
+	r.Run(":3000")
+}
