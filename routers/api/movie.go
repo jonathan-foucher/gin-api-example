289ca568ffe2e4fd41daf56f79e95dd030e8aa86
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-11-03 14:58:55 +0100

    use date type for release date

--- routers/api/movie.go
+++ routers/api/movie.go
@@ -15,12 +15,14 @@ func GetMovies(c *gin.Context) {
 
 func SaveMovie(c *gin.Context) {
 	var movie models.Movie
-	if err := c.ShouldBind(&movie); err != nil {
+	if err := c.ShouldBindJSON(&movie); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	fmt.Println("Post movie with id", movie.Id)
-	c.Status(http.StatusOK)
+	fmt.Println("Post movie with title ", movie.Title)
+	fmt.Println("Post movie with release date ", movie.ReleaseDate)
+	c.JSON(http.StatusOK, movie)
 }
 
 func DeleteMovie(c *gin.Context) {
