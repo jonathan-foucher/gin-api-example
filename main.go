73e4a5983f4fba5bfea3016ad8d5ea8e894dc158
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-04 01:00:27 +0100

    add the database connection

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	db "gin-api-example/database"
 	"gin-api-example/routers"
 )
 
 func main() {
+	conn := db.InitDbConn()
+	defer conn.Close(context.Background())
 	router := routers.InitRouter()
 	router.SetTrustedProxies(nil)
 	router.Run(":3000")
--- routers/api/movie.go
+++ routers/api/movie.go
@@ -1,36 +1,70 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"github.com/gin-gonic/gin"
-	"gin-api-example/models"
 	"strconv"
+	"github.com/gin-gonic/gin"
+	db "gin-api-example/database"
+	"gin-api-example/models"
 )
 
 func GetMovies(c *gin.Context) {
+	queries := db.New(db.GetDbConnection())
 	fmt.Println("Get all movies")
-	c.String(http.StatusOK, "Get all movies")
+
+	results, err := queries.GetMovies(context.Background())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	movies := make([]models.Movie, len(results))
+    for i, result := range results {
+        movies[i] = convertDbMovieToModelsMovie(result)
+    }
+	c.JSON(http.StatusOK, movies)
 }
 
 func SaveMovie(c *gin.Context) {
+	queries := db.New(db.GetDbConnection())
 	var movie models.Movie
 	if err := c.ShouldBindJSON(&movie); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	fmt.Println("Post movie with id", movie.Id)
 	fmt.Println("Post movie with title ", movie.Title)
 	fmt.Println("Post movie with release date ", movie.ReleaseDate)
-	c.JSON(http.StatusOK, movie)
+
+	queries.SaveMovie(context.Background(), db.SaveMovieParams{
+		ID: movie.Id,
+		Title: movie.Title,
+		ReleaseDate: movie.ReleaseDate,
+	})
+	c.Status(http.StatusOK)
 }
 
 func DeleteMovie(c *gin.Context) {
+	queries := db.New(db.GetDbConnection())
 	movieId, err := strconv.Atoi(c.Params.ByName("movieId"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	fmt.Println("Delete movie with id", movieId)
+	queries.DeleteMovie(context.Background(), int32(movieId))
 	c.Status(http.StatusOK)
+}
+
+func convertDbMovieToModelsMovie(dbMovie db.Movie) (models.Movie) {
+	movie := models.Movie{
+		Id: dbMovie.ID,
+		Title: dbMovie.Title,
+		ReleaseDate: dbMovie.ReleaseDate,
+	}
+	return movie
 }
